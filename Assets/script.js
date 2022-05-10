f6c96de7dbfbc05e0f99d78502fa5d8d58845095
eacquistapace <eacquistapace@gmail.com>
2022-05-09 19:42:01 -0600

    confirmPasswordLength return alert added

--- Assets/script.js
+++ Assets/script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-//Password criteria
+//Password criteria arrays
 var uppercase = [
   "A",
   "B",
@@ -64,7 +64,7 @@ var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 var specialCharacters = ["!", "@", "#", "$", "%", "&", "*", "?"];
 
-// Declarations
+// Variable declarations
 var confirmPasswordLength;
 var confirmUppercase;
 var confirmLowercase;
@@ -74,14 +74,13 @@ var confirmSpecialCharacters;
 //Password Length Prompt
 function generatePassword() {
   confirmPasswordLength = prompt(
-    "How many characters would you like in your password? (Must be between 8 and 128 characters)"
+    "How many characters would you like in your password? (Must be between 8 and 128 characters)."
   );
-  if (
-    generatePassword.confirmPasswordLength <= 8 &&
-    generatePassword.confirmPasswordLength >= 128
-  ) {
-    prompt("Please choose a length between 8 and 128 characters.");
+  //Password not within paramater length, will return and alert user to choose correct length.
+  if (confirmPasswordLength < 8 || confirmPasswordLength > 128) {
+    return;
   }
+  alert("Please choose a length between 8 and 128 characters.");
 }
 
 // Write password to the #password input
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Challenge-3
+
+Unfortuantely, I was unable to get very far in this challenge. I am going to resubmit when I get the prompts to work.
+
+**Screenshot**
+
+**Deployed Application**
